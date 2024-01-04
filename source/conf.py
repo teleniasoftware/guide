0e179162c0562707b3fbd2cb9fe2043b296504e1
acarradore <acarradore@teleniasoftware.com>
2024-01-04 10:55:20 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -56,8 +56,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'sphinx_rtd_theme'
-#html_theme = 'classic'
-html_theme = 'alabaster'
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
